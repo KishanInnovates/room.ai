6b7162ed378d278eed647f38cb092257e7fe637a
Kishan_Gupta <guptakishan428@gmail.com>
2024-07-27 11:26:15 +0530

    Initial Commit

--- public/page.tsx
+++ public/page.tsx
@@ -1,20 +1,27 @@
-import Header from "@/components/Header";
-import Head from "next/head";
-import Link from "next/link";
-import React from "react";
 import Image from "next/image";
-import Footer from "@/components/Footer";
+import Link from "next/link";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import SquigglyLines from "../components/SquigglyLines";
 
-
-const Home: React.FC = () => {
+export default function HomePage() {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
+        <a
+          href="https://vercel.fyi/roomGPT"
+          target="_blank"
+          rel="noreferrer"
+          className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
+        >
+          Clone and deploy your own with{" "}
+          <span className="text-blue-600">Vercel</span>
+        </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
           Generating dream rooms{" "}
           <span className="relative whitespace-nowrap text-blue-600">
-            {/* <SquigglyLines /> */}
+            <SquigglyLines />
             <span className="relative">using AI</span>
           </span>{" "}
           for everyone.
@@ -24,9 +31,11 @@ const Home: React.FC = () => {
           themes. 100% free – remodel your room today.
         </h2>
         <Link
-          href="/"
           className="bg-blue-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
-        ></Link>
+          href="/dream"
+        >
+          Generate your dream room
+        </Link>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-8 sm:flex-row flex-col">
@@ -54,9 +63,7 @@ const Home: React.FC = () => {
           </div>
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
-  
-   export default Home;
