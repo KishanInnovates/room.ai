ab88ba8ada154f9cc1309cb19875c7097deb2613
Kishan_Gupta <guptakishan428@gmail.com>
2024-08-18 09:47:27 +0530

    UploadDropZoneCompleted

--- src/app/dream/page.tsx
+++ src/app/dream/page.tsx
@@ -1,11 +1,77 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { UploadDropzone } from "@bytescale/upload-widget-react";
+import { UrlBuilder } from "@bytescale/sdk";
+import { UploadWidgetConfig } from "@bytescale/upload-widget";
+import { roomType, themeType } from "@/utils/dropdowntypes";
+
+
+const options: UploadWidgetConfig = {
+  apiKey: !!process.env.NEXT_PUBLIC_UPLOAD_API_KEY ? process.env.NEXT_PUBLIC_UPLOAD_API_KEY : "free",
+  maxFileCount: 1,
+  mimeTypes: ["image/jpeg", "image/png", "image/jpg"],
+  editor: { images: { crop: false } },
+  styles: {
+    colors: {
+      primary: "#2563EB", // Primary buttons & links
+      error: "#d23f4d", // Error messages
+      shade100: "#fff", // Standard text
+      shade200: "#fffe", // Secondary button text
+      shade300: "#fffd", // Secondary button text (hover)
+      shade400: "#fffc", // Welcome text
+      shade500: "#fff9", // Modal close button
+      shade600: "#fff7", // Border
+      shade700: "#fff2", // Progress indicator background
+      shade800: "#fff1", // File item background
+      shade900: "#ffff", // Various (draggable crop buttons, etc.)
+    },
+  },
+};
+
+
 
 export default function DreamPage() {
+  const [originalPhoto, setOriginalPhoto] = useState<String | null>(null);
+  const [photoName, setPhotoName] = useState<String | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [theme, setTheme] = useState<themeType>("Modern");
+  const [room, setRoom] = useState<roomType>("Living Room");
+
+
   const uploadDropZone = () => {
-    <UploadDropzone>
-      
-    </UploadDropzone>;
+    <UploadDropzone options={options}
+      onUpdate={({ uploadedFiles }) => {
+        if (uploadedFiles.length !== 0) {
+          const image = uploadedFiles[0];
+          const imageName = image.originalFile.originalFileName;
+          const imageUrl = UrlBuilder.url({
+            accountId: image.accountId,
+            filePath: image.filePath,
+            options: {
+              transformation: "preset",
+              transformationPreset: "thumbnail"
+            }
+          });
+          setPhotoName(imageName);
+          setOriginalPhoto(imageUrl);
+          generatePhoto(imageUrl);
+      }
+      }}
+      width="670px"
+      height="250px"
+    ></UploadDropzone>;
   };
+
+  async function generatePhoto(fileUrl : String) {
+    await new Promise((resolve) => { setTimeout(resolve, 200) });
+    setLoading(true);
+    const res = await fetch("/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ imageUrl: fileUrl, theme, room }),
+    });
+    //todo: to add function
+  }
 }
